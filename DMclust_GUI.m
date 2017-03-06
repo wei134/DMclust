c0d3fc18a139eecd54be5ee576c6cc83f294ab9b
wei134 <weizegan@msu.edu>
2017-03-06 16:00:01 +0800

    Add files via upload

---
+++ DMclust_GUI.m
@@ -0,0 +1,280 @@
+function varargout = DMclust_GUI(varargin)
+% MTMD_GUI MATLAB code for MtMd_GUI.fig
+%      MTMD_GUI, by itself, creates a new MTMD_GUI or raises the existing
+%      singleton*.
+%
+%      H = MTMD_GUI returns the handle to a new MTMD_GUI or the handle to
+%      the existing singleton*.
+%
+%      MTMD_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MTMD_GUI.M with the given input arguments.
+%
+%      MTMD_GUI('Property','Value',...) creates a new MTMD_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before MtMd_GUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to MtMd_GUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help MtMd_GUI
+
+% Last Modified by GUIDE v2.5 06-Mar-2017 11:45:32
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @MtMd_GUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @MtMd_GUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before MtMd_GUI is made visible.
+function MtMd_GUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to MtMd_GUI (see VARARGIN)
+
+% Choose default command line output for MtMd_GUI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes MtMd_GUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = MtMd_GUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[pname,adrname]=uigetfile();
+set(handles.edit1,'String',[adrname,pname]);
+
+% guidata(hObject,handles);
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+% set(hObject,'String',ss);
+% guidata(hObject,handles);
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+addd=get(handles.edit1,'String');
+thta=get(handles.edit2,'String');
+thta=str2num(thta);
+[headers seqs] = fastaread(addd);  % 读取序列
+[motif_is,motif]=motif_hh2(seqs,1-thta);                    % 搜索motif
+net=net_motif(motif_is);     % 根据motif构建网络
+hebing_motif=net_q(motif,net,0.98);  % 用Q挖掘
+otu=shengyu(hebing_motif,seqs);  % 将剩余节点归入
+xieru(addd,otu,headers);
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on selection change in popupmenu2.
+function popupmenu2_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu2 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu2
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in popupmenu5.
+function popupmenu5_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu5 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu5
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --------------------------------------------------------------------
+function Untitled_1_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Untitled_2_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Untitled_3_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Untitled_4_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Untitled_5_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Citation_Callback(hObject, eventdata, handles)
+% hObject    handle to Citation (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function Untitled_8_Callback(hObject, eventdata, handles)
+% hObject    handle to Untitled_8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function About_DMclust_Callback(hObject, eventdata, handles)
+% hObject    handle to About_DMclust (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+open('./private/About_DMclust.fig');
+
+% --------------------------------------------------------------------
+function Help_Callback(hObject, eventdata, handles)
+% hObject    handle to Help (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+open('./private/Help.fig');
+
+
+% --------------------------------------------------------------------
+function Copyright_Callback(hObject, eventdata, handles)
+% hObject    handle to Copyright (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+open('./private/Copyright.fig');
+
+
+% --------------------------------------------------------------------
+function Citation1_Callback(hObject, eventdata, handles)
+% hObject    handle to Citation1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+open('./private/Citation.fig')
---
+++ readme.txt
@@ -0,0 +1,67 @@
+====================================================================
+		    
+ DMclust_GUI                             2016/04/18
+ ver1.0.0                           ICI/LIFT, China
+		    	 	       
+====================================================================
+
+System Requirements
+===================
+
+Runtime Environment Requirements
+--------------------------------
+Matlab 7.12.0 or later
+
+
+Software Requirements
+---------------------
+The DMclust_GUI is supported on the following operating systems 
+   
+   Windows 7
+   Windows 8 
+
+Hardware Requirements/Recommendations
+-------------------------------------
+   Intel Core i3 2100 or later for Windows 
+   Color display capable of 1024 X 768 pixel resolution
+   
+
+Memory Requirements/Recommendations
+-------------------------------------
+DMclust_GUI requires approximately
+200 M of available disk space on the drive or partition.
+
+DMclust_GUI requires a minimum of
+2G of RAM for clustering 16S rRNA dataset.
+
+
+User's Guide
+=================
+
+Input Sequences Requirements
+----------------------------
+Input file is *.txt and input sequences require FASTA format, e.g.
+ >E7SG6HO01EHCN6 
+ AAACACTTGACATGTTCGTCGCGACTCTAAGAGATTAGAGTTTTCGGTTCGGCCGGACGAAACAC
+ >E7SG6HO01EJNYR
+ AAATCTCGACATCCTCTGACCCCTCTAGAGATAGAGTTTTCCCCTTCGGGGGACAGAGTGAC
+
+Execute Step
+------------
+Step 1: Open Matlab.
+Step 2: Change 'Current Folder' to the 'DMclust_GUI' folder.
+Step 3: Double click DMclust_GUI.m.
+Step 4: Press 'F5' key to run DMclust_GUI.m then the window of DMclust_GUI is opened.
+Step 5: Click 'Upload' button to upload sequence file.
+Step 6: Set threshold theta.
+Step 7: Click 'Run' button.
+Step 8: The final clustering results are presented in text formats (*_DMclust.txt) and can be found in the input file directory.
+
+
+Copyright Notice
+===================
+Software, documentation and related materials:
+Copyright (c) 2017-2019 
+Institute of Control & Information(ICI), Northwestern Polytechnical University,China
+Key Laboratory of Information Fusion Technology(LIFT), Ministry of Education, China
+All rights reserved.
